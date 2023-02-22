60b370de335b0a505c1db008103f501acf6e23d2
hina <matsunohinako@matsunoakonombp.lan>
2023-02-22 22:02:38 +0900

    Add counting flags

--- cli/greet.go
+++ cli/greet.go
@@ -9,16 +9,50 @@ import (
 )
 
 func main() {
+    // args()
+    // flagSample()
+    boolCount()
+}
+
+func boolCount(){
+    var count int
+
+    app := &cli.App{
+        Flags: []cli.Flag{
+            &cli.BoolFlag{
+                Name:        "foo",
+                Usage:       "foo greeting",
+                Count: &count,
+            },
+        },
+        Action: func(cCtx *cli.Context) error {
+            fmt.Println("count", count)
+            return nil
+        },
+    }
+
+    // greet --foo --foo --foo
+    // count 3
+
+    if err := app.Run(os.Args); err != nil {
+        log.Fatal(err)
+    }
+}
+
+func flagSample(){
+    var language string
+
     app := &cli.App{
         Flags: []cli.Flag{
             &cli.StringFlag{
                 Name:  "lang",
                 Value: "english",
                 Usage: "language for the greeting",
+                Destination: &language,
             },
         },
         Action: func(cCtx *cli.Context) error {
-            name := "Nefertiti"
+            name := "someone"
             if cCtx.NArg() > 0 {
                 name = cCtx.Args().Get(0)
             }
@@ -30,6 +64,26 @@ func main() {
             return nil
         },
     }
+
+    // greet --lang=jp taro
+    // こんにちは taro
+
+    if err := app.Run(os.Args); err != nil {
+        log.Fatal(err)
+    }
+}
+
+// 引数
+func args(){
+    app := &cli.App{
+        Action: func(cCtx *cli.Context) error {
+            fmt.Printf("Hello %q", cCtx.Args().Get(0))
+            return nil
+        },
+    }
+
+    // greet hello
+    // hello 
 
     if err := app.Run(os.Args); err != nil {
         log.Fatal(err)
